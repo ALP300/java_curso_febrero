9804134ebe2865da5785b28523caef981dc0d944
ALP300 <aitorpalomino1@gmail.com>
2025-02-05 18:38:45 -0500

    Subir proyecto

---
+++ src/main/java/com/mycompany/java_curso_febrero/Java_curso_febrero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_curso_febrero;
+
+/**
+ *
+ * @author AXEL
+ */
+public class Java_curso_febrero {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
